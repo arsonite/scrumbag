7430426f5e12b1341bdfae4511b4e85de6893a95
arsonite <notarson@gmail.com>
2019-09-01 15:30:35 +0200

    added custom boilerplater

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import * as serviceWorker from './services/serviceWorker';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+import { DEBUG } from './config.js';
+
+import './style/index.css';
+
+if (!DEBUG) {
+	console.warn = () => {};
+	console.error = () => {};
+}
+
+ReactDOM.render(
+	<React.Fragment></React.Fragment>,
+	document.getElementById('root')
+);
+
 serviceWorker.unregister();
